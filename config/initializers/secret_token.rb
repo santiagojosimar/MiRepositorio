0bb54504b78111d2361c97bd120e3e4db134391a
JadeSoft <jadesoft5@gamil.com>
2013-09-20 17:25:48 -0400

    my first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pensamientos::Application.config.secret_token = '077388490f5d29d5a8ae708b69c53d376c79991bfeb2ccc483739f0add31267d39d21c7bcf1ea0a17da34a4d104ef5b6f7c1963710ba202c5c0d9a3edf98d89a'
